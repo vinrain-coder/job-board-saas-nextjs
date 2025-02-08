d69e78d6846ffcd7f2fc40c577f84257d6e9e319
vinrain-coder <vincentombogo57@gmail.com>
2025-02-08 17:27:28 +0000

    add company schema

--- components/forms/onboarding/CompanyForm.tsx
+++ components/forms/onboarding/CompanyForm.tsx
@@ -1,6 +1,27 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { companySchema } from "@/utils/zodSchemas";
+import { z } from "zod";
+import { Form } from "@/components/ui/form";
 
-export  function CompanyForm() {
+export function CompanyForm() {
+  const form = useForm<z.infer<typeof companySchema>>({
+    resolver: zodResolver(companySchema),
+    defaultValues: {
+      about: "",
+      location: "",
+      logo: "",
+      name: "",
+      website: "",
+      xAccount: "",
+    },
+  });
+
   return (
-    <div>CompanyForm</div>
+    <Form {...form}>
+        <form className="space-y-6">
+            <div className="grid grid-cols-1 "></div>
+        </form>
+    </Form>
   )
 }
