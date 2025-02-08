b558ffc70cd6c55e60e5d059c5c84088e7235700
vinrain-coder <vincentombogo57@gmail.com>
2025-02-08 12:10:57 +0000

    Set app layout and add dark theme

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Navbar } from "@/components/general/Navbar";
+import { ThemeProvider } from "@/components/general/ThemeProvider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Jobber | Find and Post Jobs",
+  description: "A place where you can find and at the same time post jobs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
+            <Navbar />
+            {children}
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/general/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Logo from "@/public/logo.png";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { ThemeToggle } from "./ThemeToggle";
+
+export function Navbar() {
+  return (
+    <nav className="flex items-center justify-between py-5">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src={Logo} alt="Logo" width={40} height={40} />
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-primary">ber</span>
+        </h1>
+      </Link>
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <Button>Login</Button>
+      </div>
+    </nav>
+  );
+}
