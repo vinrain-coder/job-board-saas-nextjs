52cf806322afe25352be3d2f5de33446eab958e0
vinrain-coder <vincentombogo57@gmail.com>
2025-02-09 14:58:34 +0000

    update the nav bar

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Logo from "@/public/logo.png";
 import Image from "next/image";
-import { Button, buttonVariants } from "../ui/button";
+import { buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./ThemeToggle";
-import { auth, signOut } from "@/utils/auth";
+import { auth } from "@/utils/auth";
+import { UserDropdown } from "./UserDropdown";
 
 export async function Navbar() {
   const session = await auth();
@@ -16,21 +17,24 @@ export async function Navbar() {
           Job<span className="text-primary">ber</span>
         </h1>
       </Link>
-      <div className="flex items-center gap-4">
+
+      {/* Desktop nav */}
+
+      <div className="hidden md:flex items-center gap-5">
         <ThemeToggle />
+        <Link className={buttonVariants({ size: "lg" })} href="/post-job">
+          Post Job
+        </Link>
         {session?.user ? (
-          <form
-            action={async () => {
-              "use server";
-              await signOut({ redirectTo: "/" });
-            }}
-          >
-            <Button>Logout</Button>
-          </form>
+          <UserDropdown
+            email={session.user.email as string}
+            image={session.user.image as string}
+            name={session.user.name as string}
+          />
         ) : (
           <Link
-            href="/login"
             className={buttonVariants({ variant: "outline", size: "lg" })}
+            href="/login"
           >
             Login
           </Link>
---
+++ components/general/UserDropdown.tsx
@@ -0,0 +1,72 @@
+import { ChevronDown, Heart, Layers2, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import Link from "next/link";
+import { signOut } from "@/utils/auth";
+
+interface iAppProps {
+  email: string;
+  name: string;
+  image: string;
+}
+
+export function UserDropdown({ email, name, image }: iAppProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-auto p-0 hover:bg-transparent">
+          <Avatar>
+            <AvatarImage src={image} alt="Profile-image" />
+            <AvatarFallback>{name.charAt(0)}</AvatarFallback>
+          </Avatar>
+          <ChevronDown size={16} strokeWidth={2} className="ml-2 opacity-60" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-48" align="end">
+        <DropdownMenuLabel className="flex flex-col">
+          <span className="text-sm font-medium text-foreground">{name}</span>
+          <span className="text-xs text-muted-foreground">{email}</span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href="/favorites">
+              <Heart size={16} strokeWidth={2} className="opacity-60" />
+              <span>Favorite jobs</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/my-jobs">
+              <Layers2 size={16} strokeWidth={2} className="opacity-60" />
+              <span>My job listings</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild>
+          <form
+            action={async () => {
+              "use sever";
+
+              await signOut({ redirectTo: "/" });
+            }}
+          >
+            <button className="flex items-center w-full gap-2">
+              <LogOut size={16} strokeWidth={2} className="opacity-60" />
+              <span>Logout</span>
+            </button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
