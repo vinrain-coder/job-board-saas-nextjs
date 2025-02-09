7d2747ed8e62612413bd4eb383b0fd7794d77806
vinrain-coder <vincentombogo57@gmail.com>
2025-02-09 06:45:13 +0000

    add company form

--- components/forms/onboarding/CompanyForm.tsx
+++ components/forms/onboarding/CompanyForm.tsx
@@ -2,7 +2,25 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { companySchema } from "@/utils/zodSchemas";
 import { z } from "zod";
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Globe } from "lucide-react";
 
 export function CompanyForm() {
   const form = useForm<z.infer<typeof companySchema>>({
@@ -19,9 +37,58 @@ export function CompanyForm() {
 
   return (
     <Form {...form}>
-        <form className="space-y-6">
-            <div className="grid grid-cols-1 "></div>
-        </form>
+      <form className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter company name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company location</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select location" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Worldwwide</SelectLabel>
+                      <SelectItem value="worldwide">
+                        <span>
+                          <Globe />
+                        </span>
+                        <span>Worldwide / Remote</span>
+                      </SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Location</SelectLabel>
+                      
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
     </Form>
-  )
+  );
 }
