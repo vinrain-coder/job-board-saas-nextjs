91bcc73497da1086da19145f4cea2b782c8bbc74
vinrain-coder <vincentombogo57@gmail.com>
2025-02-09 14:30:52 +0000

    secure app with arcjet

--- app/actions.ts
+++ app/actions.ts
@@ -5,9 +5,32 @@ import { requireUser } from "./utils/requireUser";
 import { companySchema, jobSeekerSchema } from "@/utils/zodSchemas";
 import { prisma } from "@/utils/db";
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "./utils/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: "LIVE",
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  );
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const session = await requireUser();
+
+  const req = await request();
+
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
 
   const validateData = companySchema.parse(data);
 
@@ -31,6 +54,14 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
 
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const user = await requireUser();
+
+  const req = await request();
+
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("Forbidden");
+  }
 
   const validateData = jobSeekerSchema.parse(data);
 
