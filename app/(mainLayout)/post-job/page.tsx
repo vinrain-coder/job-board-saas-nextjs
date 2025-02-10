a7a19f20dc5c55d16b9787e186f82cf07a1b1dc4
Vinrain <143264723+vinrain-coder@users.noreply.github.com>
2025-02-10 20:26:47 +0300

    Update page.tsx

--- app/(mainLayout)/post-job/page.tsx
+++ app/(mainLayout)/post-job/page.tsx
@@ -77,7 +77,7 @@ const PostJobPage = async () => {
   const session = await requireUser();
   const data = await getCompany(session.id as string);
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5 mb-4">
       <CreateJobForm
         companyAbout={data.about}
         companyLocation={data.location}
